8944e8e1670ed7a5ad22774e8358505269009aad
Maria Prudnikova <C:\Users\Marsha\AppData\Roaming\The Bat!>
2019-03-24 15:17:10 +0300

    feat: write function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  var stack = [];
+  for (var i = 0; i < str.length; i++) {
+    for (var j = 0; j < bracketsConfig.length; j++) {
+      if (bracketsConfig[j][0] == bracketsConfig[j][1]) {
+        if (str[i] == bracketsConfig[j][0]) {
+          if (stack.length > 0 && j == stack[stack.length-1]) {
+            stack.pop();
+            break;
+          } else {
+            stack.push(j);
+            break;
+          }
+        }
+      } else {
+        if (str[i] == bracketsConfig[j][0]) {
+          stack.push(j);
+          break;
+        } else {
+          if (str[i] == bracketsConfig[j][1]) {
+            if (stack.length > 0 && j == stack[stack.length-1]) {
+              stack.pop();
+              break;
+            } else {
+              return false;
+            }
+          }
+        }
+      }
+    }
+  }
+  if (stack.length == 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
